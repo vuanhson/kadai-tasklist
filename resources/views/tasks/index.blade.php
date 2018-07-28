f66b92b062b3e9c3b2427b9f47cd363e4cbc00cb
vusonhust <vusonhust@outlook.com>
2018-07-28 05:44:59 +0000

    sign in and sign up

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,26 +3,38 @@
 @section('content')
     
 <!-- ここにページ毎のコンテンツを書く -->
-    {!! link_to_route('tasks.create', 'Create new task', null, ['class' => 'btn btn-primary']) !!}
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>Task content</th>
-                    <th>Task status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
+    @if (Auth::check())
+        <?php $user = Auth::user(); ?>
+        <p><h3>Welcome {{ $user->name }}, here is your tasklist:</h3></p>
+        
+        {!! link_to_route('tasks.create', 'Create new task', null, ['class' => 'btn btn-primary']) !!}
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
                     <tr>
-                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                        <td>{{ $task->content }}</td>
-                        <td>{{ $task->status }}</td>
+                        <th>id</th>
+                        <th>Task content</th>
+                        <th>Task status</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
-
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                        <tr>
+                            <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                            <td>{{ $task->content }}</td>
+                            <td>{{ $task->status }}</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+    
+        @endif
+    @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Welcome to the Tasklist</h1>
+                {!! link_to_route('signup.get', 'Sign up now!', null, ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
 @endsection
